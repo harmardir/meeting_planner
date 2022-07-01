02f68af1147c68d5ee9172bc1c4dc4a506db7fd4
Harout Mardirossian <harmardir@users.noreply.github.com>
2022-07-01 23:25:36 +0300

    creating meetings app and model

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -8,3 +8,7 @@ def welcome(request):
 
 def date(request):
     return HttpResponse("This page was served at: " + str(datetime.now()))
+
+
+def about(request):
+    return HttpResponse("This is about me page")
