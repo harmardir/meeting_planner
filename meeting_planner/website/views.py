11c38528f803ce3c7f7bf22ec7167074d66f15eb
Harout Mardirossian <harmardir@users.noreply.github.com>
2022-07-04 23:42:39 +0300

    adding for loop to the template

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -2,9 +2,11 @@ from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from meetings.models import Meeting
 
 def welcome(request):
-    return render(request, "website/welcome.html")
+    return render(request, "website/welcome.html",
+    {"meetings":Meeting.object.all()})
 
 def date(request):
     return HttpResponse("This page was served at: " + str(datetime.now()))
